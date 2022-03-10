f6520d3b115bc8caa6cfa7820f70f9ff21260558
AquibTx1 <99241917+AquibTx1@users.noreply.github.com>
2022-03-10 16:21:39 +0530

    created appservice

--- main.tf
+++ main.tf
@@ -37,3 +37,17 @@ resource "azurerm_app_service_plan" "qa_plan_appservice" {
     size = "S1"
   }
 }
+resource "azurerm_app_service" "dev-aquib-appservice" {
+  name                = "dev-aquib-appservice"
+  location            = azurerm_resource_group.rg_aquib_dev.location
+  resource_group_name = azurerm_resource_group.rg_aquib_dev.name
+  app_service_plan_id = azurerm_app_service_plan.dev_plan_appservice.id
+
+}
+resource "azurerm_app_service" "qa-aquib-appservice" {
+  name                = "qa-aquib-appservicee"
+  location            = azurerm_resource_group.rg_aquib_qa.location
+  resource_group_name = azurerm_resource_group.rg_aquib_qa.name
+  app_service_plan_id = azurerm_app_service_plan.qa_plan_appservice.id
+
+}
