0f13b620cf8a66094718acb45f298965cfa58ce3
AquibTx1 <99241917+AquibTx1@users.noreply.github.com>
2022-03-10 15:51:35 +0530

    resource group created

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "=2.91.0"
     }
   }
 }
 provider "azurerm" {
   features {}
+}
+resource "azurerm_resource_group" "rg_aquib_dev" {
+  name     = "rg_aquib_dev"
+  location = "Central India"
+}
+resource "azurerm_resource_group" "rg_aquib_qa" {
+  name     = "rg_aquib_qa"
+  location = "Central India"
 }
